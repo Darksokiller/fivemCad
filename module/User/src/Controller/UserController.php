ba937df5f8a1853fb06b6d405500eea783f28ba1
Evan Smith <eevansocom@gmail.com>
2022-02-08 19:44:07 -0600

    set up the user module

---
+++ module/User/src/Controller/UserController.php
@@ -0,0 +1,12 @@
+<?php
+namespace User\Controller;
+
+use Laminas\Mvc\Controller\AbstractActionController;
+
+class UserController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        
+    }
+}
